e6ea47b368b4646c677daeb2b66c4dea3b7de523
정민호 <allmin9702@naver.com>
2025-02-12 16:01:40 +0900

    feat: 주소 Entity 수정

--- src/main/java/com/example/springserver/domain/entity/Sido.java
+++ src/main/java/com/example/springserver/domain/entity/Sido.java
@@ -8,21 +8,20 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "copyoflocation2")
-public class Copyoflocation2 {
-    @Id
-    @Column(name = "location_id", nullable = false)
-    private Long id;
+public class Sido {
 
-    @Size(max = 255)
+    @Id
     @NotNull
-    @ColumnDefault("특별시,광역시, 시, 도")
-    @Column(name = "do", nullable = false)
-    private String doField;
+    @Column(nullable = false)
+    private Long sido_id;
+
+    @Size(max = 20)
+    @NotNull
+    @Column(nullable = false)
+    private String sido_name;
 
 }
