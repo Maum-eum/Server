03c8ef5fcf53cdaff85a70712557e04e3f62c6f8
정민호 <allmin9702@naver.com>
2025-02-12 16:01:40 +0900

    feat: 주소 Entity 수정

--- src/main/java/com/example/springserver/domain/entity/Sigungu.java
+++ src/main/java/com/example/springserver/domain/entity/Sigungu.java
@@ -3,7 +3,6 @@ package com.example.springserver.domain.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -13,27 +12,19 @@ import org.hibernate.annotations.ColumnDefault;
 @Getter
 @Setter
 @Entity
-public class Location {
-    @Id
-    @Column(nullable = false)
-    private Long location_id;
+public class Sigungu {
 
-    @NotNull
+    @Id
     @Column(nullable = false)
     private Long sigungu_id;
 
-    @Size(max = 20)
     @NotNull
     @Column(nullable = false)
-    private String dong_name;
-
-    @Size(max = 20)
-    @NotNull
-    @Column(nullable = false)
-    private String sido_name;
+    private Long sido_id;
 
     @Size(max = 20)
     @NotNull
     @Column(nullable = false)
     private String sigungu_name;
+
 }
