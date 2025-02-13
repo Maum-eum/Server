c9b4abb38aa1708bcfa5266456135bed957fff4a
정민호 <allmin9702@naver.com>
2025-02-13 16:50:01 +0900

    feat: 요양보호사 회원가입시 자격증 및 경력 추가기능 구현

--- src/main/java/com/example/springserver/domain/caregiver/controller/CaregiverController.java
+++ src/main/java/com/example/springserver/domain/caregiver/controller/CaregiverController.java
@@ -2,36 +2,35 @@ package com.example.springserver.domain.caregiver.controller;
 
 import com.example.springserver.domain.caregiver.converter.CaregiverConverter;
 import com.example.springserver.domain.caregiver.entity.Caregiver;
-import com.example.springserver.domain.caregiver.dto.request.CaregiverRequestDTO;
-import com.example.springserver.domain.caregiver.dto.response.CaregiverResponseDTO;
+import com.example.springserver.domain.caregiver.service.CareGiverService;
+import com.example.springserver.domain.caregiver.service.ExperienceService;
 import com.example.springserver.service.JoinService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.example.springserver.domain.caregiver.dto.request.CaregiverRequestDTO.*;
+import static com.example.springserver.domain.caregiver.dto.response.CaregiverResponseDTO.*;
+
 @RestController
 @Validated
 @Tag(name = "Caregiver API", description = "Caregiver에 해당하는 요양 보호사에 대한 API")
 @RequestMapping("/caregiver")
+@RequiredArgsConstructor
 public class CaregiverController {
 
     private final JoinService joinService;
 
-    public CaregiverController (JoinService joinService) {
-        this.joinService = joinService;
-    }
-
     @Operation(summary = "회원가입", description = "Post")
     @PostMapping("/signup")
-    public CaregiverResponseDTO.SignUpCaregiverResult signUpCaregiver(@RequestBody @Valid CaregiverRequestDTO.SignUpCaregiverReq request){
-
+    public SignUpCaregiverResult signUpCaregiver(@RequestBody @Valid SignUpCaregiverReq request){
         Caregiver newCaregiver = joinService.signUpCaregiver(request);
-
         return CaregiverConverter.toSignUpCaregiverResult(newCaregiver);
     }
 }
