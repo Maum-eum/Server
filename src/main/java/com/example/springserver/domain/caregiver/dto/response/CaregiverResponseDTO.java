f5ebcc1f90b2f9706ce49f810ce2e7cb8cc44c1c
정민호 <allmin9702@naver.com>
2025-02-14 03:08:26 +0900

    feat: 구직 조건 생성시 반환 DTO 구현

--- src/main/java/com/example/springserver/domain/caregiver/dto/response/CaregiverResponseDTO.java
+++ src/main/java/com/example/springserver/domain/caregiver/dto/response/CaregiverResponseDTO.java
@@ -3,6 +3,7 @@ package com.example.springserver.domain.caregiver.dto.response;
 import com.example.springserver.domain.caregiver.dto.request.CaregiverRequestDTO;
 import com.example.springserver.domain.caregiver.entity.enums.CertType;
 import com.example.springserver.domain.caregiver.entity.enums.Level;
+import com.example.springserver.domain.caregiver.entity.enums.Week;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -10,6 +11,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class CaregiverResponseDTO {
@@ -50,6 +52,79 @@ public class CaregiverResponseDTO {
         private List<CertificateResponseDTO> certificateResponseDTOList;
 
         private List<ExperienceResponseDTO> experienceResponseDTOList ;
+    }
+
+    @Getter
+    @Builder
+    public static class JobConditionResponseDTO {
+
+        private Boolean flexibleSchedule = false;
+
+        private Boolean desiredHourlyWage = false;
+
+        private Boolean selfFeeding = false;
+
+        private Boolean mealPreparation = false;
+
+        private Boolean cookingAssistance = false;
+
+        private Boolean enteralNutritionSupport = false;
+
+        private Boolean selfToileting = false;
+
+        private Boolean occasionalToiletingAssist = false;
+
+        private Boolean diaperCare = false;
+
+        private Boolean catheterOrStomaCare = false;
+
+        private Boolean independentMobility = false;
+
+        private Boolean mobilityAssist = false;
+
+        private Boolean wheelchairAssist = false;
+
+        private Boolean immobile = false;
+
+        private Boolean cleaningLaundryAssist = false;
+
+        private Boolean bathingAssist = false;
+
+        private Boolean hospitalAccompaniment = false;
+
+        private Boolean exerciseSupport = false;
+
+        private Boolean emotionalSupport = false;
+
+        private Boolean cognitiveStimulation = false;
+
+        private List<LocationResponseDTO> locationRequestDTOList;
+
+        private List<WorkTimeResponseDTO> workTimeRequestDTOList;
+    }
+
+    @Getter
+    @Builder
+    public static class LocationResponseDTO{
+
+        @NotNull
+        private String locationName;
+
+    }
+
+    @Getter
+    @Builder
+    public static class WorkTimeResponseDTO{
+
+        @NotNull
+        private Week dayOfWeek;
+
+        @NotNull
+        private LocalDateTime start_time;
+
+        @NotNull
+        private LocalDateTime end_time;
+
     }
 
     @Getter
