4486acdb7c144378a2a054e4719e68256a177bd9
Hszoo <trre1827151@gmail.com>
2025-02-15 18:14:21 +0900

    chore: 서버 실행시 센터 데이터 삽입

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5351,19 +5351,3 @@ FROM (
 ) AS v(dong_name, sido_name, sigungu_name)
 JOIN Sigungu sg ON sg.sigungu_name = v.sigungu_name
 JOIN Sido s ON s.sido_name = v.sido_name AND sg.sido_id = s.sido_id;
-
-
-INSERT INTO Center (center_id, leaderId, centerName, hasBathCar, rate, intro, startTime, endTime, address)
-VALUES (1, 101, '행복 요양 센터', true, 'A', '편안한 노후를 위한 최상의 서비스', '08:00', '18:00', '서울특별시 강남구 1길 10');
-
-INSERT INTO Center (center_id, leaderId, centerName, hasBathCar, rate, intro, startTime, endTime, address)
-VALUES (2, 102, '사랑 요양 센터', false, 'B', '정성을 다하는 요양 서비스', '09:00', '17:00', '서울특별시 서초구 2길 20');
-
-INSERT INTO Center (center_id, leaderId, centerName, hasBathCar, rate, intro, startTime, endTime, address)
-VALUES (3, 103, '희망 요양 센터', true, 'A', '최신 시설을 갖춘 쾌적한 환경', '07:30', '19:00', '부산광역시 해운대구 3길 30');
-
-INSERT INTO Center (center_id, leaderId, centerName, hasBathCar, rate, intro, startTime, endTime, address)
-VALUES (4, 104, '온정 요양 센터', true, 'C', '가족 같은 분위기의 요양원', '08:30', '18:30', '대전광역시 유성구 4길 40');
-
-INSERT INTO Center (center_id, leaderId, centerName, hasBathCar, rate, intro, startTime, endTime, address)
-VALUES (5, 105, '편안한 요양 센터', false, 'B', '친절하고 따뜻한 케어', '09:00', '16:00', '인천광역시 연수구 5길 50');
