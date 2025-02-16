8eb2e9e5d59ba12dbaf650681550dd28b4188ce7
Hszoo <trre1827151@gmail.com>
2025-02-16 19:09:04 +0900

    fix: 엘라스틱 서치 적용 해제

--- src/main/java/com/example/springserver/domain/center/controller/CenterController.java
+++ src/main/java/com/example/springserver/domain/center/controller/CenterController.java
@@ -1,31 +1,30 @@
-//package com.example.springserver.domain.center.controller;
-//
-//import com.example.springserver.domain.center.entity.CenterDocument;
-//import com.example.springserver.domain.center.service.CenterService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.data.web.PageableDefault;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping("/center")
-//public class CenterController {
-//
-//    private final CenterService centerService;
-//
-//    @GetMapping("/search")
-//    public Page<CenterDocument> searchCenters(
-//            @RequestParam String keyword,
-//            @PageableDefault(size = 10) Pageable pageable) {
-//
-//        return centerService.searchCenters(keyword, pageable.getPageNumber(), pageable.getPageSize());
-//    }
-//}
+package com.example.springserver.domain.center.controller;
+
+import com.example.springserver.domain.center.converter.CenterConverter;
+import com.example.springserver.domain.center.dto.response.CenterResponseDto;
+import com.example.springserver.domain.center.dto.response.CenterResponseDto.CenterSearchDto;
+import com.example.springserver.domain.center.entity.Center;
+import com.example.springserver.domain.center.service.CenterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/center")
+public class CenterController {
+
+    private final CenterService centerService;
+
+    @GetMapping("/search")
+    public List<CenterSearchDto> searchCenters(
+            @RequestParam String keyword) {
+
+        List<Center> centers = centerService.searchCenterName(keyword);
+        return CenterConverter.toSearchListDto(centers);
+    }
+}
