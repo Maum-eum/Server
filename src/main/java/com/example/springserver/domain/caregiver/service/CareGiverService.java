2b5910e9876a58f1ed8c6f33168f97ec2ad9d08d
정민호 <allmin9702@naver.com>
2025-02-17 23:39:38 +0900

    refactor: 요양보호사 정보수정 API 수정

--- src/main/java/com/example/springserver/domain/caregiver/service/CareGiverService.java
+++ src/main/java/com/example/springserver/domain/caregiver/service/CareGiverService.java
@@ -1,7 +1,10 @@
 package com.example.springserver.domain.caregiver.service;
 
+import com.example.springserver.domain.caregiver.converter.CaregiverConverter;
 import com.example.springserver.domain.caregiver.dto.request.CaregiverRequestDto.*;
 import com.example.springserver.domain.caregiver.entity.Caregiver;
+import com.example.springserver.domain.caregiver.entity.Certificate;
+import com.example.springserver.domain.caregiver.entity.Experience;
 import com.example.springserver.domain.caregiver.repository.CaregiverRepository;
 import com.example.springserver.global.apiPayload.format.ErrorCode;
 import com.example.springserver.global.apiPayload.format.GlobalException;
@@ -9,6 +12,8 @@ import com.example.springserver.global.security.util.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -27,7 +32,17 @@ public class CareGiverService {
     public Caregiver updateUserInfo(CustomUserDetails user,
                                     UpdateCaregiverReqDto request) {
         Caregiver caregiver = getById(user);
-        caregiver.setUpdate(caregiver,request);
+
+        updateCertificates(caregiver, request.getCertificateRequestDTOList());
+        updateExperiences(caregiver, request.getExperienceRequestDTOList());
+
+        caregiver.setCar(request.getCar());
+        caregiver.setAddress(request.getAddress());
+        caregiver.setImg(request.getImg());
+        caregiver.setEducation(request.getEducation());
+        caregiver.setContact(request.getContact());
+        caregiver.setIntro(request.getIntro());
+
         return caregiverRepository.save(caregiver);
     }
 
@@ -40,5 +55,34 @@ public class CareGiverService {
 
     private Caregiver getById(CustomUserDetails user) throws GlobalException {
         return caregiverRepository.findById(user.getId()).orElseThrow(()-> new GlobalException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    private void updateCertificates(Caregiver caregiver, List<CertificateRequestDTO> newCertificates) {
+        List<Certificate> updatedCertificates = newCertificates.stream()
+                .map(dto -> CaregiverConverter.toCertificate(caregiver, dto))
+                .toList();
+
+        for (Certificate newCert : updatedCertificates) {
+
+            if (!caregiver.getCertificates().contains(newCert)) {
+                caregiver.getCertificates().add(newCert);
+            }
+        }
+
+        caregiver.getCertificates().removeIf(cert -> !updatedCertificates.contains(cert));
+    }
+
+    private void updateExperiences(Caregiver caregiver, List<ExperienceRequestDTO> newExperiences) {
+        List<Experience> updatedExperiences = newExperiences.stream()
+                .map(dto -> CaregiverConverter.toExperience(caregiver, dto))
+                .toList();
+
+        for (Experience newExp : updatedExperiences) {
+            if (!caregiver.getExperiences().contains(newExp)) {
+                caregiver.getExperiences().add(newExp);
+            }
+        }
+
+        caregiver.getExperiences().removeIf(exp -> !updatedExperiences.contains(exp));
     }
 }
