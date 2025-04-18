2704b2fa3bddf13975d8b35a75e39374f4341897
Hszoo <trre1827151@gmail.com>
2025-04-18 16:17:06 +0900

    chore: Caffeine Cache 사용을 위한 세팅- #120

---
+++ src/main/java/com/example/springserver/global/cache/CacheType.java
@@ -0,0 +1,16 @@
+package com.example.springserver.global.cache;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum CacheType {
+    RECRUIT_CONDITION_CACHE("recruit_condition", 12, 10000),
+    JOB_CONDITION_CACHE("job_condition", 12, 10000),
+    MATCH_SCORE_CACHE("match_score", 12, 10000);
+
+    private final String cacheName;
+    private final int expiredAfterWrite;
+    private final int maximumSize;
+}
